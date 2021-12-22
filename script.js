de80fccb762518cb56bc038c695cb77693f4d570
devyosse <narutoshippuden884@gmail.com>
2021-12-22 13:56:36 +0100

    exo 122

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $(".hole");
+const scoreBoard = $('.score');
+//const moles = $(".mole");
+let finish = 0;
+const btnStart = $("#startGame");
+
+btnStart.click(function () {
+    finish = 0;
+    StartGame();
+    time();
+
+    holes.click(function () {
+        if (finish !== 1) {
+            if ($(this).hasClass("up")) {
+                scoreBoard.text(parseFloat(scoreBoard.text()) + 1);
+                $(this).removeClass("up");
+            }
+        }
+    })
+})
+
+function StartGame() {
+    let hole1 = holes[random(6, 0)];
+    hole1.classList.add("up");
+    time();
+
+    setTimeout(function () {
+        if(hole1.classList.contains("up")) {
+            hole1.classList.remove("up");
+        }
+
+        StartGame();
+    }, random(700, 200))}
+
+
+function random(number, numberTwo) {
+    return (Math.trunc(Math.random() * number) + numberTwo);
+}
+
+function time() {
+    let timer = 0;
+
+    function chrono() {
+        let chrono = setTimeout(function () {
+            timer++;
+            time();
+        }, 1000)
+        if (timer === 10) {
+            clearTimeout(chrono);
+            if (holes.hasClass("up")) {
+                holes.removeClass("up");
+            }
+            return finish++;
+        }
+    }
+    chrono()
+}
+
+time();
+
+
+
+
+
